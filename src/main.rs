f2b66127c0c5322b4980938d9dddc0a2200bedf4
Matthew Sainsbury <msainsbury@microsoft.com>
2023-02-07 14:49:00 -0800

    Add more messages

--- src/main.rs
+++ src/main.rs
@@ -41,7 +41,16 @@ const MESSAGES: &'static [&'static str] = &[
     "Do you devote too much time to scrolling Mastodon? Why don't you try <ACTIVITY> instead?",
     "Hello, I know you are doing your best. Why don't you try <ACTIVITY> instead of scrolling on here?",
     "Is scrolling on Mastodon making your life better? Maybe <ACTIVITY> would be better.",
-    "Sometimes I use social media when I'm feeling disconnected from the world. Maybe you could make plans with someone, or try <ACTIVITY>"
+    "Sometimes I use social media when I'm feeling disconnected from the world. Maybe you could make plans with someone, or try <ACTIVITY>",
+    "Scrolling social media can give us a false sense of agency in our own lives. Why don't you exercise some real agency by <ACTIVITY>",
+    "There are other things to experience in life other than this app. Why don't you try <ACTIVITY> instead",
+    "I made this account because it's too easy for me to spend hours scrolling on here. Maybe it's the same for you. I think that our lives are better spent on other things, like <ACTIVITY>.",
+    "Very little will come from spending your time on social media. You might get more out of <ACTIVITY>.",
+    "Hello, I'm here to remind you to stop using social media excessively. <ACTIVITY> might be a better use of time.",
+    "Leave social media for the politicians and the chronically angry. You might have a better time <ACTIVITY>.",
+    "There's nothing wrong with using this app, but have you considered <ACTIVITY> instead?",
+    "Hey! Maybe you could take a break?",
+    "Make sure you control the apps you use and not the other way around."
 ];
 
 const BETTER_ACTIVITIES: &'static [&'static str] = &[
@@ -52,7 +61,14 @@ const BETTER_ACTIVITIES: &'static [&'static str] = &[
     "reflecting on your day or planning for tomorrow",
     "going on a walk",
     "planning a trip",
-    "going to a park"
+    "going to a park",
+    "doing some tidying around the house",
+    "writing down five things that are going well",
+    "closing your eyes and focusing on your breathing for a minute",
+    "getting some exercise for fun",
+    "doing a craft project",
+    "going out and buying yourself a small treat",
+    "getting ice cream"
 ];
 
 fn generate_message() -> String {
